2d8c49869a3dd2ca0a55c6bd06f83be5fd66645d
RaoFurqan524 <raofurqan524@gmail.com>
2024-07-26 03:22:26 +0500

    Add files via upload

---
+++ global_variables.dart
@@ -0,0 +1,34 @@
+final products = [
+  {
+    'id': '0',
+    'title': 'Men\'s Nike Shoes',
+    'Price': 44.52,
+    'imageUrl': 'assets/fonts/images/shoes_1.png',
+    'company': 'Nike',
+    'sizes': [9,10,11,12],
+  },
+    {
+    'id': '1',
+    'title': 'Addidas Shoes',
+    'Price': 20.12,
+    'imageUrl': 'assets/fonts/images/shoes_2.png',
+    'company': 'Addidas',
+    'sizes': [9,10,11],
+  },
+    {
+    'id': '2',
+    'title': 'Bata Women\'s Shoes',
+    'Price': 28.95,
+    'imageUrl': 'assets/fonts/images/shoes_3.png',
+    'company': 'Bata',
+    'sizes': [8,9,10],
+  },
+    {
+    'id': '3',
+    'title': 'Jordan Shoes',
+    'Price': 420.69,
+    'imageUrl': 'assets/fonts/images/shoes_4.png',
+    'company': 'Nike',
+    'sizes': [8,9,10],
+  },
+];
